13a25f45aa7011c02dd74e41d0e381bd36506822
wenjiadong <wenjiadong@gaosiedu.com>
2018-06-10 17:45:25 +0800

    new file

---
+++ tianjin/src/main/java/com/earth/tianjin/TianjinApplication.java
@@ -0,0 +1,12 @@
+package com.earth.tianjin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TianjinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TianjinApplication.class, args);
+    }
+}
